e12b94cb6317e89f1f2afd944528acecd59c12ee
Daniel Ko <lawsus@users.noreply.github.com>
2023-09-28 01:16:50 -0500

    Create file_system_manager

---
+++ bot/file_system_manager.py
@@ -0,0 +1,109 @@
+import os
+import subprocess
+
+
+class FileSystemManager:
+    def __init__(self):
+        # user id to file system
+        self.file_system = {}
+        # user id to current paths
+        self.current_paths = {}
+
+    def _get_current_directory(self, path):
+        current_folder = self.file_system
+        for folder in path:
+            if folder in current_folder:
+                current_folder = current_folder[folder]
+            else:
+                return None
+        return current_folder
+
+    def _get_current_path(self, user_id):
+        # default file system and current directory
+        if user_id not in self.file_system:
+            self.file_system[user_id] = {}
+        if user_id not in self.current_paths:
+            self.current_paths[user_id] = [user_id]
+        # get current path
+        return self.current_paths.get(user_id)
+
+    def write_file(self, user_id, file_name, content):
+        # navigate to current directory using current path
+        current_path = self._get_current_path(user_id)
+        current_directory = self._get_current_directory(current_path)
+
+        # add file name and content to current directory
+        current_directory[file_name] = content
+
+    def read_file(self, user_id, file_name):
+        # navigate to current directory using current path
+        current_path = self._get_current_path(user_id)
+        current_directory = self._get_current_directory(current_path)
+
+        # return code from file name
+        return current_directory.get(file_name, "File not found")
+
+    def run_file(self, user_id, file_name):
+        code = self.read_file(user_id, file_name)
+        if code == "File not found":
+            return code
+
+        with open(f"temp_{user_id}_{file_name}", "w") as temp_file:
+            temp_file.write(code)
+
+        try:
+            output = subprocess.check_output(
+                ["python3", f"temp_{user_id}_{file_name}"],
+                stderr=subprocess.STDOUT,
+                text=True,
+            )
+            os.remove(f"temp_{user_id}_{file_name}")
+            return output
+        except subprocess.CalledProcessError as e:
+            os.remove(f"temp_{user_id}_{file_name}")
+            return e.output
+
+    def list_directories(self, user_id):
+        # navigate to current directory using current path
+        current_path = self._get_current_path(user_id)
+        current_directory = self._get_current_directory(current_path)
+
+        # return files/folders in current directory
+        return list(current_directory.keys())
+
+    def make_directory(self, user_id, dir_name):
+        # navigate to current directory using current path
+        current_path = self._get_current_path(user_id)
+        current_directory = self._get_current_directory(current_path)
+
+        if dir_name not in current_directory:
+            current_directory[dir_name] = {}
+            return f"Directory {dir_name} created successfully"
+        else:
+            return f"Directory {dir_name} already exists"
+
+    def change_directory(self, user_id, path):
+        # navigate to current directory using current path
+        current_path = self._get_current_path(user_id)
+        current_directory = self._get_current_directory(current_path)
+
+        if path.strip() == "..":
+            if len(current_path) > 1:
+                current_path.pop()
+                if len(current_path) == 1:
+                    return "Changed to root directory successfully"
+                else:
+                    return f'Changed to directory {"/".join(current_path[1:])} successfully'
+            else:
+                return f"Already at root directory"
+        else:
+            path_elements = path.split("/")
+            for folder in path_elements:
+                if folder in current_directory and isinstance(
+                    current_directory[folder], dict
+                ):
+                    current_directory = current_directory[folder]
+                else:
+                    return f'Directory {"/".join(path_elements)} does not exist'
+            current_path.extend(path_elements)
+            return f'Changed to directory {"/".join(path_elements)} successfully'
---
+++ bot/main.py
@@ -0,0 +1,94 @@
+# discord imports
+import discord
+from discord.ext import commands
+
+# get env
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# import file system manager
+from file_system_manager import FileSystemManager
+
+# get discord token
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+# initialize bot
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!p3 ", intents=intents)
+
+# initialize file system manager
+file_system_manager = FileSystemManager()
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+
+@bot.command()
+async def echo(ctx, arg):
+    print("Echo command received")
+    await ctx.send(arg)
+
+
+@bot.command()
+async def write(ctx, file_name: str, *, code: str):
+    # clean up string
+    code = code.replace("```python", "").replace("```", "").strip()
+
+    # write file
+    user_id = ctx.message.author.id
+    file_system_manager.write_file(user_id, file_name, code)
+
+    await ctx.send(f"File `{file_name}` has been written successfully.")
+
+
+@bot.command()
+async def read(ctx, file_name: str):
+    # read file
+    user_id = ctx.message.author.id
+    content = file_system_manager.read_file(user_id, file_name)
+
+    await ctx.send(f"```python\n{content}```")
+
+
+@bot.command()
+async def run(ctx, file_name: str):
+    # run file
+    user_id = ctx.message.author.id
+    output = file_system_manager.run_file(user_id, file_name)
+
+    await ctx.send(f"```python\n{output}```")
+
+
+@bot.command()
+async def ls(ctx):
+    # list directories
+    user_id = ctx.message.author.id
+    directories = file_system_manager.list_directories(user_id)
+
+    await ctx.send(f'Directories: {", ".join(directories)}')
+
+
+@bot.command()
+async def mkdir(ctx, dir_name: str):
+    # make directories
+    user_id = ctx.message.author.id
+    output = file_system_manager.make_directory(user_id, dir_name)
+
+    await ctx.send(f"{output}")
+
+
+@bot.command()
+async def cd(ctx, path: str):
+    # change directories
+    user_id = ctx.message.author.id
+    output = file_system_manager.change_directory(user_id, path)
+
+    await ctx.send(f"{output}")
+
+
+bot.run(DISCORD_TOKEN)
